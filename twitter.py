370717d28bcf0b3e1508923db44bf3c666b32da7
Thibault Févry <ThibaultFevry@gmail.com>
2015-03-02 00:42:27 +0100

    Now we get the ids of users who liked big data. Testing this with our 10Mb gives 1100 clients (may be duplicates) from 2500 tweets, which either says people are very enthusiastic abot big data or we are too quick to say a tweet is positive.

--- twitter.py
+++ twitter.py
@@ -4,24 +4,31 @@ import json
 
 # Recovering and preprocessing tweets
 
+# It's easier to make 2 lists than alist of list, especially for debugging
+# purposes.
+tweets = []
+authors = []
+clients = []
+
 # This is commented out, don't remove, needed to document steps taken
 # tweets = open("obama_tweets.txt").read()
 
 # Json loader bugs with very large files. Therefore, we will parse it line
 # by line. Don't try to use load() to gain space, it won't work.
 
-tweets = []
 with open("tweets", "r") as parsed_tweets:
     for line in parsed_tweets:
-        # So far we just need the text.
-        tweets.append(json.loads(line)['text'])
+        # Need to save it to avoid calling json.loads twice
+        tweet = json.loads(line) 
+        tweets.append(tweet['text'])
+        authors.append(tweet['user']['id'])
 
 # Standardizing tweets to enhance detection rate
 tweets = [t.lower() for t in tweets]
 for p in punctuation:
     tweets = [t.replace(p, '') for t in tweets]
 
-print tweets
+#print tweets, authors
 
 # Setting up lists to check whether a file is good or not
 with open("positive.txt", "r") as pos:
@@ -31,7 +38,7 @@ with open("negative.txt", "r") as neg:
     negative_words = neg.read().splitlines()
 
 # Processing tweets
-for tweet in tweets:
+for i, tweet in enumerate(tweets):
     positive_counter, negative_counter = 0, 0
 
     words = tweet.split(' ')
@@ -41,4 +48,8 @@ for tweet in tweets:
         elif word in negative_words:
             negative_counter += 1
 
-    print positive_counter/len(words), ' ', negative_counter/len(words)
+    # FIXME This might be too leniant a definition of a positive tweet
+    if positive_counter > negative_counter:
+        clients.append(authors[i])
+
+
