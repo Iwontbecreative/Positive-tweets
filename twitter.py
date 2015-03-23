8de5da0537b5692a96f48ee36eb129052c613279
Thibault Févry <ThibaultFevry@gmail.com>
2015-03-23 12:19:00 +0100

    Rewrite preprocessing tweets as a function so it can be called from other files

--- twitter.py
+++ twitter.py
@@ -2,33 +2,30 @@ from __future__ import division
 from string import punctuation
 import json
 
-# Recovering and preprocessing tweets
-
-# It's easier to make 2 lists than alist of list, especially for debugging
-# purposes.
 tweets = []
 authors = []
-clients = []
+prospects = []
 
-# This is commented out, don't remove, needed to document steps taken
-# tweets = open("obama_tweets.txt").read()
+def preprocess(tweet):
+    """
+    Takes a tweet and preprocesses it so as to enhance detection rate.
+    """
+    tweet = tweet.lower()
+    for p in punctuation:
+        tweet = tweet.replace(p, '')
+    return tweet
 
 # Json loader bugs with very large files. Therefore, we will parse it line
 # by line. Don't try to use load() to gain space, it won't work.
 
-with open("tweets", "r") as parsed_tweets:
+with open("test", "r") as parsed_tweets:
     for line in parsed_tweets:
         # Need to save it to avoid calling json.loads twice
-        tweet = json.loads(line) 
+        tweet = json.loads(line)
         tweets.append(tweet['text'])
         authors.append(tweet['user']['id'])
 
-# Standardizing tweets to enhance detection rate
-tweets = [t.lower() for t in tweets]
-for p in punctuation:
-    tweets = [t.replace(p, '') for t in tweets]
-
-#print tweets, authors
+tweets = [preprocess(t) for t in tweets]
 
 # Setting up lists to check whether a file is good or not
 with open("positive.txt", "r") as pos:
@@ -50,6 +47,7 @@ for i, tweet in enumerate(tweets):
 
     # FIXME This might be too leniant a definition of a positive tweet
     if positive_counter > negative_counter:
-        clients.append(authors[i])
+        prospects.append(authors[i])
 
-
+with open("potential clients2.txt", "a") as c:
+    c.write("\n".join([str(i) for i in prospects]))
