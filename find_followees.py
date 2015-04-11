c8c8cf53d776a68637fd47b3fa6613ceb0bb6962
Thibault Févry <ThibaultFevry@gmail.com>
2015-04-11 18:13:35 +0200

    Add default keywords so as to improve results quality (we upped about_topic default too)

--- find_followees.py
+++ find_followees.py
@@ -59,7 +59,8 @@ def scan_tweets(keywords, number=1000, output_file='tweets'):
 
 if __name__ == '__main__':
     keywords = ['big data', 'machine learning', 'deep learning', 'hadoop',
-                'data mining', 'open data', 'mapreduce', 'nosql']
+                'data mining', 'open data', 'mapreduce', 'nosql', 'analytics',
+                'mongodb', 'cassandra', 'analytics']
     parser = argparse.ArgumentParser(description="This is a twitter-based prospect finder tool")
     parser.add_argument('-k', '--keywords', dest='keywords',
                         help='Keywords that we should scan twitter for.')
