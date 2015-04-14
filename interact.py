765ae22eddc0c197c41456155ca2aef47c93472b
Thibault Févry <ThibaultFevry@gmail.com>
2015-04-14 23:59:59 +0200

    Make interact.py a script. Check that we only retweet stuff about big data and not randomly as before (retweeting quran was weird for a big data stream).

--- interact.py
+++ interact.py
@@ -7,30 +7,30 @@ import tweepy
 import positive
 import random
 import os
+import argparse
 from credentials import our_id
 
-def choose_tweet(api, id):
+
+def choose_tweet(api, id, keywords):
     """
     Given some userid, choose which tweet should be acted upon. To do that,
-    chose the tweet Twitter users liked the most."""
-    # Count = 10 to avoid retweeting old things so we're less robot-like
-    tweets = api.user_timeline(user_id=id, count=10)
-    most_popular = max(t.retweet_count + t.favorite_count for t in tweets)
-    for t in tweets:
-        if t.retweet_count + t.favorite_count == most_popular:
-            return t.id
+    chose the tweet Twitter users liked the most.
+    """
+    tweets = api.user_timeline(user_id=id, count=15)
+    topic_tweets = [t for t in tweets if any(True for k in keywords if k in positive.preprocess(t.text))]
+    if topic_tweets:
+        most_popular = max(t.retweet_count + t.favorite_count for t in topic_tweets)
+        for t in tweets:
+            if t.retweet_count + t.favorite_count == most_popular:
+                return t.id
+    return
 
-def pick_prospect(api):
-    """
-    So far we just choose at random.
-    """
-    return random.choice(get_prospect_list(api, False))
 
 def get_prospect_list(api=None, refresh=False):
-    """Builds a list of prospects from Twitter if refresh, else uses the cached one.
-    This is our id : 3130995473.
-    Twitter's doc states that we will receive last followed people first, but
-    that this behaviour is subject to change.
+    """
+    Builds a list of prospects from Twitter if refresh, else uses the cached
+    one. Twitter's doc states that we will receive last followed people first,
+    but that this behaviour is subject to change.
     """
     if refresh:
         os.remove('prospects')
@@ -47,14 +47,34 @@ def random_action(api):
     return random.choice((api.create_favorite, api.retweet))
 
 if __name__ == '__main__':
+    keywords = ['big data', 'machine learning', 'deep learning', 'hadoop',
+                'data mining', 'open data', 'mapreduce', 'nosql', 'analytics',
+                'mongodb', 'cassandra', 'analytics']
+    parser = argparse.ArgumentParser(description="Interact with prospects.")
+    parser.add_argument('-k', '--keywords', dest='keywords',
+                        help='Keywords that we should scan twitter for.')
+    parser.add_argument('-a', '--action_number', default=20, dest='number',
+                        help='How many actions we should do before stopping')
+    args = parser.parse_args()
+    if args.keywords:
+        keywords = args.keywords.split(', ')
+    number = int(args.number)
+
     api = tweepy.API(positive.setup_auth())
-    for i in range(7):
-        print(i)
-        tweet_id = choose_tweet(api, pick_prospect(api))
-        #FIXME: Understand why sometimes next line throws a 403.
+    prospect_list = get_prospect_list(api, refresh=True)
+    i = 0
+    while i < number:
+        tweet_id = choose_tweet(api, random.choice(prospect_list), keywords)
+        # Sometimes no_retweet is specified.
         try:
-            a = random_action(api)
-            a(tweet_id)
+            if tweet_id:
+                action = random_action(api)
+                action(tweet_id)
+                i += 1
+                print("Action %s taken" % i)
+        except tweepy.error.TweepError:
+            print('Tweet number %s is not retweetable' % tweet_id)
+            continue
         except:
-            print(a, tweet_id)
+            print('Another error, probably hit the limit.')
             break
