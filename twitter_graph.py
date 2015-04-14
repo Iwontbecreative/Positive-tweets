301103879bb1e57bdd912a8032363f12790dbc1e
Thibault Févry <ThibaultFevry@gmail.com>
2015-04-14 19:44:10 +0200

    Complete our twitter graph with edges. It not works, aside from us not being centered.

--- twitter_graph.py
+++ twitter_graph.py
@@ -8,13 +8,11 @@ Draws a graph of the network of our Twitter account. Shows:
 
 import networkx as nx
 import matplotlib.pyplot as plt
-import interact
 import tweepy
 import positive
 from credentials import our_id
 
 
-### Now we create a class inheriting from DiGraph
 class prospect_map(nx.DiGraph):
     """
     A graph of prospects. Even if we retweet stuff from other people or that
@@ -22,7 +20,8 @@ class prospect_map(nx.DiGraph):
     """
     def __init__(self, api):
         nx.DiGraph.__init__(self)
-        self.followed = set(api.friends_ids()) 
+        # We add ourself to simplify things.
+        self.followed = set(api.friends_ids() + [our_id])
         self.favorited = set(i.user.id for i in api.favorites(count=200)) & self.followed
         self.retweeted = set(i.user.id for i in api.home_timeline(count=200)) & self.followed
         self.followers = set(api.followers_ids()) & self.followed
@@ -35,38 +34,37 @@ class prospect_map(nx.DiGraph):
         -> Users we did not interact with and that followed us -> ff
         -> Users we interacted with and that followed us. -> ffi
         """
-        prospect_map.add_nodes_from(self.followed)
+        # We also add our id because it will be needed for edges.
+        prospect_map.add_nodes_from(self, self.followed)
         f = self.followed - (self.favorited | self.retweeted | self.followers)
         fi = (self.favorited | self.retweeted) - self.followers
         ff = self.followers - (self.favorited & self.retweeted)
         ffi = self.followers & (self.favorited | self.retweeted)
         print(len(f), len(fi), len(ff), len(ffi))
 
-    def printer(self):
-        print(len(self.followed))
+        self.pos = nx.circular_layout(self)
 
-        pos = nx.circular_layout(self)
-       
+        nx.draw_networkx_nodes(self, self.pos, nodelist=f, labels=False,
+                               node_size=1, node_color='#000000')
+        nx.draw_networkx_nodes(self, self.pos, nodelist=fi, labels=False,
+                               node_size=20, node_color='#ccff00')
+        nx.draw_networkx_nodes(self, self.pos, nodelist=ff, labels=False,
+                               node_size=50, node_color='#ff6600')
+        nx.draw_networkx_nodes(self, self.pos, nodelist=ffi, labels=False,
+                               node_size=100, node_color='#990000')
 
-        nx.draw_networkx_nodes(self, pos, nodelist=f, labels=False, node_size=1,
-                          #  node_color='#000000')
-        nx.draw_networkx_nodes(self, pos, nodelist=fi, labels=False, node_size=5,
-                          #  node_color='#ccff00')
-        nx.draw_networkx_nodes(self, pos, nodelist=ff, labels=False, node_size=10,
-                            node_color='#ff6600')
-        nx.draw_networkx_nodes(self, pos, nodelist=ffi, labels=False, node_size=100,
-                            node_color='#990000')
-
-    
-
-def construct_edges():
-    """
-    Get the list of people who follow us.
-    """
-    api = tweepy.API(positive.setup_auth())
-    return set((i, our_id) for i in api.followers_ids(user_id=our_id))
+    def construct_edges(self):
+        """
+        Add links from us to people we interacted with.
+        Add links from people to us when they followed us.
+        """
+        prospect_map.add_edges_from(self, set((i, our_id) for i in self.followers))
+        prospect_map.add_edges_from(self, set((our_id, i) for i in self.retweeted | self.favorited))
+        nx.draw_networkx_edges(self, self.pos)
 
 api = tweepy.API(positive.setup_auth())
 pm = prospect_map(api)
 pm.construct_nodes()
+pm.construct_edges()
+
 plt.show()
