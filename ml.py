e7eb0b02b64d4115d6512bd18233a681336792d0
Thibault Févry <ThibaultFevry@gmail.com>
2015-04-20 15:01:01 +0200

    Start working on supervised learning for ml.py. Will probably require some NLP so holding back on that front for now.

--- ml.py
+++ ml.py
@@ -3,56 +3,91 @@ The goal of this programme is to get a taste of machine learning through the
 data we collected. We'll try to do both supervised and unsupervised learning.
 """
 
+import numpy as np
+import random
 import tweepy
 from positive import setup_auth
 from pandas import DataFrame as df
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-import numpy as np
+from sklearn.cross_validation import train_test_split
+
+
+
+lin_reg = 0 #True
+forest_reg = False
+supervised = True
+unsupervised = False
 
 api = tweepy.API(setup_auth())
 
-# We need to save someone who is popular and has a lot of tweets to have
-# meaningful results.
-friends = api.friends(count=200)
-maximum_friends = max(i.followers_count for i in friends)
-print(maximum_friends)
-for f in friends:
-    if f.followers_count == maximum_friends:
-        user = f.id
-        print(f.name)
-        break
 
-user_tweets = [t.__dict__ for t in api.user_timeline(user_id=user, count=200)]
+def linear_regression(dataset):
+    model = LinearRegression(normalize=True)
+    X = dataset.retweet_count[:, np.newaxis]
+    model.fit(X, dataset.favorite_count)
+    max_value = max(max(dataset.favorite_count), max(dataset.retweet_count))
 
-ds = df(user_tweets)
+    # Plotting
+    xx = np.linspace(0, max_value, 100)
+    yy = xx
+    yy = yy * model.coef_ + model.intercept_
+    print(model.residues_, model.coef_, model.intercept_)
+    plt.plot(xx, yy)
+    plt.show()
 
-# Keep only the rows we need for analysis.
-ds = ds[['id', 'text', 'created_at', 'lang', 'retweet_count',
-         'favorite_count']]
-ds.text = ds.text.apply(lambda t: len(t))
 
-plt.scatter(ds.retweet_count, ds.favorite_count)
+def forest_regression(dataset):
+    model = RandomForestRegressor()
+    max_value = max(max(dataset.favorite_count), max(dataset.retweet_count))
+    x_fit = np.linspace(0, max_value, 100)[:, np.newaxis]
+    y_fit = model.predict(x_fit)
+    print(model.residues)
+    plt.plot(X.squeeze())
+    plt.plot(x_fit.squeeze(), y_fit)
+    plt.show()
 
-# Implement a small linear regression
-model = LinearRegression(normalize=True)
-model = RandomForestRegressor()
-X = ds.retweet_count[:, np.newaxis]
-model.fit(X, ds.favorite_count)
-max_value = max(max(ds.favorite_count), max(ds.retweet_count))
+if lin_reg or forest_reg:
+    # We need to save someone who is popular and has a lot of tweets to have
+    # meaningful results. Unfortunately, even with MongoDB (157k followers)
+    # our regression does not work that well.
+    friends = api.friends(count=200)
+    maximum_friends = max(i.followers_count for i in friends)
+    print(maximum_friends)
+    for f in friends:
+        if f.followers_count == maximum_friends:
+            user = f.id
+            print(f.name)
+            break
+    user = 813286  # Obama's id.
 
-# Linear regression plotting
-#xx = np.linspace(0, max_value, 100)
-#yy = xx
-#yy = yy * model.coef_ + model.intercept_
-#print(model.residues_, model.coef_, model.intercept_)
-#plt.plot(xx, yy)
-#plt.show()
+    user_tweets = [t.__dict__ for t in api.user_timeline(user_id=user, count=200)]
 
-# RandomForest plotting
-x_fit = np.linspace(0, max_value, 100)[:, np.newaxis]
-y_fit = model.predict(x_fit)
-plt.plot(X.squeeze())
-plt.plot(x_fit.squeeze(), y_fit)
-plt.show()
+    ds = df(user_tweets)
+    # Remove non-retweetable tweets as they obvisouly skew results.
+    # Since we're analyzing someone popular, if a tweet is never retweeted it
+    # must mean that it is not retweetable
+    ds = ds[ds.retweet_count >= 1]
+
+    # Keep only the columns we need for analysis.
+    ds = ds[['id', 'text', 'created_at', 'lang', 'retweet_count',
+             'favorite_count']]
+    ds.text = ds.text.apply(lambda t: len(t))
+    plt.scatter(ds.retweet_count, ds.favorite_count)
+    if lin_reg:
+        linear_regression(ds)
+    if forest_reg:
+        forest_regression(ds)
+
+if supervised:
+    friends = api.friends_ids()
+    to_test = [random.choice(friends), random.choice(friends), random.choice(friends)]
+    print(to_test)
+    user_tweets = [api.user_timeline(user_id=id, count=200) for id in to_test]
+    tweet_dicts = [[t.__dict__ for t in u] for u in user_tweets]
+    tweets = [tweet for sublist in tweet_dicts for tweet in sublist]
+    ds = df(tweets)
+
+    ds = ds[['user', 'text', 'created_at', 'lang', 'retweet_count']]
+    ds.head()
